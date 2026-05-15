25250941c44e7d150f1050923c9059bf5640e1c3
Shreyash Nathe <shreyashnathe2005@gmail.com>
2026-05-15 10:39:59 +0530

    Final

--- Dijkstra.java
+++ Dijkstra.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Dijkstra {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -18,7 +19,23 @@ public class Dijkstra {
             }
         }
 
-        System.out.print("Enter source vertex: ");
+        // Print Graph
+        System.out.println("\nGraph Representation:");
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (g[i][j] != 0) {
+
+                    System.out.println(
+                            i + " --(" + g[i][j] + ")--> " + j
+                    );
+                }
+            }
+        }
+
+        System.out.print("\nEnter source vertex: ");
         int s = sc.nextInt();
 
         int[] d = new int[n];
@@ -28,13 +45,17 @@ public class Dijkstra {
 
         d[s] = 0;
 
+        // Dijkstra Algorithm
         for (int c = 0; c < n - 1; c++) {
 
             int u = -1;
             int mn = Integer.MAX_VALUE;
 
+            // Find minimum distance vertex
             for (int i = 0; i < n; i++) {
+
                 if (!vis[i] && d[i] < mn) {
+
                     mn = d[i];
                     u = i;
                 }
@@ -42,9 +63,11 @@ public class Dijkstra {
 
             vis[u] = true;
 
+            // Update distances
             for (int v = 0; v < n; v++) {
 
-                if (!vis[v] && g[u][v] != 0 &&
+                if (!vis[v] &&
+                        g[u][v] != 0 &&
                         d[u] != Integer.MAX_VALUE &&
                         d[u] + g[u][v] < d[v]) {
 
@@ -53,29 +76,54 @@ public class Dijkstra {
             }
         }
 
-        System.out.println("Shortest distances:");
+        // Print shortest distances
+        System.out.println("\nShortest Distances:");
 
         for (int i = 0; i < n; i++) {
-            System.out.println(s + " -> " + i + " = " + d[i]);
+
+            System.out.println(
+                    s + " --> " + i + " = " + d[i]
+            );
         }
     }
 }
 
 
-// Enter number of vertices: 5
+/*
 
-// Enter adjacency matrix:
-// 0 10 0 30 100
-// 10 0 50 0 0
-// 0 50 0 20 10
-// 30 0 20 0 60
-// 100 0 10 60 0
+INPUT
 
-// Enter source vertex: 0
+Enter number of vertices: 5
 
-// Shortest distances:
-// 0 -> 0 = 0
-// 0 -> 1 = 10
-// 0 -> 2 = 50
-// 0 -> 3 = 30
-// 0 -> 4 = 60
+Enter adjacency matrix:
+0 10 0 30 100
+10 0 50 0 0
+0 50 0 20 10
+30 0 20 0 60
+100 0 10 60 0
+
+
+GRAPH REPRESENTATION
+
+0 --(10)--> 1
+0 --(30)--> 3
+0 --(100)--> 4
+1 --(50)--> 2
+2 --(20)--> 3
+2 --(10)--> 4
+3 --(60)--> 4
+
+
+OUTPUT
+
+Enter source vertex: 0
+
+Shortest Distances:
+
+0 --> 0 = 0
+0 --> 1 = 10
+0 --> 2 = 50
+0 --> 3 = 30
+0 --> 4 = 60
+
+*/
--- PrimMST.java
+++ PrimMST.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class PrimMST {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of vertices: ");
@@ -17,6 +19,18 @@ public class PrimMST {
             }
         }
 
+        // Printing Graph
+        System.out.println("\nGraph Representation:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (g[i][j] != 0) {
+                    System.out.println(i + " --(" + g[i][j] + ")--> " + j);
+                }
+            }
+        }
+
         boolean[] vis = new boolean[n];
         int[] key = new int[n];
         int[] par = new int[n];
@@ -26,12 +40,15 @@ public class PrimMST {
         key[0] = 0;
         par[0] = -1;
 
+        // Prim's Algorithm
         for (int c = 0; c < n - 1; c++) {
 
             int u = -1;
             int mn = Integer.MAX_VALUE;
 
+            // Find minimum key vertex
             for (int i = 0; i < n; i++) {
+
                 if (!vis[i] && key[i] < mn) {
                     mn = key[i];
                     u = i;
@@ -40,8 +57,13 @@ public class PrimMST {
 
             vis[u] = true;
 
+            // Update adjacent vertices
             for (int v = 0; v < n; v++) {
-                if (g[u][v] != 0 && !vis[v] && g[u][v] < key[v]) {
+
+                if (g[u][v] != 0 &&
+                        !vis[v] &&
+                        g[u][v] < key[v]) {
+
                     key[v] = g[u][v];
                     par[v] = u;
                 }
@@ -50,28 +72,46 @@ public class PrimMST {
 
         int cost = 0;
 
-        System.out.println("Edges in MST:");
+        System.out.println("\nEdges in MST:");
 
         for (int i = 1; i < n; i++) {
-            System.out.println(par[i] + " - " + i + " : " + g[i][par[i]]);
+
+            System.out.println(
+                    par[i] + " --(" + g[i][par[i]] + ")--> " + i
+            );
+
             cost += g[i][par[i]];
         }
 
-        System.out.println("Total Cost = " + cost);
+        System.out.println("\nTotal Cost = " + cost);
     }
 }
 
-// Enter number of vertices: 4
 
-// Enter adjacency matrix:
-// 0 2 0 6
-// 2 0 3 8
-// 0 3 0 0
-// 6 8 0 0
+/*
+INPUT
 
-// Edges in MST:
-// 0 - 1 : 2
-// 1 - 2 : 3
-// 0 - 3 : 6
+Enter number of vertices: 4
 
-// Total Cost = 11
+Enter adjacency matrix:
+0 2 0 6
+2 0 3 8
+0 3 0 0
+6 8 0 0
+
+
+OUTPUT
+
+Graph Representation:
+0 --(2)--> 1
+0 --(6)--> 3
+1 --(3)--> 2
+1 --(8)--> 3
+
+Edges in MST:
+0 --(2)--> 1
+1 --(3)--> 2
+0 --(6)--> 3
+
+Total Cost = 11
+*/
